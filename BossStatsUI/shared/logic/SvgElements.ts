607060ab37fbfc00d96905f6f54cb79ee30e9d68
Dietmar Schoder <dietmar@schoder.uk>
2024-08-07 12:51:42 +0100

    F30-90

--- BossStatsUI/shared/logic/SvgElements.ts
+++ BossStatsUI/shared/logic/SvgElements.ts
@@ -1,8 +1,12 @@
 import { OneToOneQuartiles } from "../models/FuehrrStats";
 
 var grey = "rgb(230,232,232)";
-var q = "rgb(155,96,155)";
-var q2 = "rgb(96,55,96)";
+var qBad = "rgb(255, 31, 31)";
+var qMedium = "rgb(255,191,31)";
+var qGood = "rgb(31,255,63)";
+var q2Bad = "rgb(235, 15, 15)";
+var q2Medium = "rgb(235,171,15)";
+var q2Good = "rgb(15,235,15)";
 var lineHeight = 24;
 var margin2 = 6;
 
@@ -103,14 +107,22 @@ export class SvgQuartile extends SvgElement {
         let max = this._quartiles.maximum * factor;
         let w1 = q1 - min > 1 ? q1 - min - 1 : 2;
         let w2 = q3 - q1 > 1 ? q3 - q1 - 1 : 2;
-        let w3 = max - q3 > 1 ? max - q3 - 1 : 2;
+        let w3 = max - q3 > 1 ? max - q3 : 0;
         let med = this._quartiles.median * factor;
-        return `<text alignment-baseline=\"middle\" x=\"${this.x}\" y=\"${this.y + this.height / 2 + 1}\">
-            ${this._quartiles.minimum} ${this._quartiles.q1} ${this._quartiles.median} ${this._quartiles.q3} ${this._quartiles.maximum} 
-            </text>
-            <rect x=\"${this.x + min}\" y=\"${this.y}\" width=\"${w1}\" height=\"${this.height}\" fill=\"${q}\" stroke-width=\"0\" />
-            <rect x=\"${this.x + q1}\" y=\"${this.y}\" width=\"${w2}\" height=\"${this.height}\" fill=\"${q}\" stroke-width=\"0\" />
-            <rect x=\"${this.x + q3}\" y=\"${this.y}\" width=\"${w3}\" height=\"${this.height}\" fill=\"${q}\" stroke-width=\"0\" />
-            <rect x=\"${this.x + med}\" y=\"${this.y - 2}\" width=\"${2}\" height=\"${this.height + 4}\" fill=\"${q2}\" stroke-width=\"0\" />`;
+
+        let colourMin = this._quartiles.minimum > 2.66 ? qGood : this._quartiles.minimum > 1.33 ? qMedium : qBad;
+        let colourQ1 = this._quartiles.q1 > 2.66 ? qGood : this._quartiles.q1 > 1.33 ? qMedium : qBad;
+        let colourQ3 = this._quartiles.q3 > 2.66 ? qGood : this._quartiles.q3 > 1.33 ? qMedium : qBad;
+        let colourMed = this._quartiles.median > 2.66 ? q2Good : this._quartiles.median > 1.33 ? q2Medium : q2Bad;
+
+        return `
+            <rect x=\"${this.x + min}\" y=\"${this.y}\" width=\"${w1}\" height=\"${this.height}\" fill=\"${colourMin}\" stroke-width=\"0\" />
+            <rect x=\"${this.x + q1}\" y=\"${this.y}\" width=\"${w2}\" height=\"${this.height}\" fill=\"${colourQ1}\" stroke-width=\"0\" />
+            <rect x=\"${this.x + q3}\" y=\"${this.y}\" width=\"${w3}\" height=\"${this.height}\" fill=\"${colourQ3}\" stroke-width=\"0\" />
+            <rect x=\"${this.x + med}\" y=\"${this.y - 2}\" width=\"${2}\" height=\"${this.height + 4}\" fill=\"${colourMed}\" stroke-width=\"0\" />`
+            //+ `<text alignment-baseline=\"middle\" x=\"${this.x}\" y=\"${this.y + this.height / 2 + 1}\">
+            //${this._quartiles.minimum} ${this._quartiles.q1} ${this._quartiles.median} ${this._quartiles.q3} ${this._quartiles.maximum} 
+            //</text>`
+            ;
     }
 }
